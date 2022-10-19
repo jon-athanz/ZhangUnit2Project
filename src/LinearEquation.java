b6880075280c107b85eb1ac29dcc2d2c670be659
BT_3W2_05 <jonathanz28@nycstudents.net>
2022-10-19 09:30:31 -0400

    ZhangUnit2Project

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+
+    /* Instance Variables */
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public double distance() {
+        double distance = Math.round((Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2))) / 100.0) * 100.0;
+        return distance;
+    }
+
+    public double yIntercept() {
+        double yIntercept = Math.round((y1 - (slope() * x1)) / 100.0) * 100.0;
+        return yIntercept;
+    }
+
+    public double slope() {
+        double slope = Math.round((y2 - y1) / (x2 - x1) / 100.0) * 100.0;
+        return slope;
+    }
+
 }
+
+
+
+
+
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class LinearEquationRunner {
+import java.util.Scanner;
+
+public class LinearEquationRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome!");
+        System.out.println("Enter coordinate 1: i.e. (x, y)");
+        String first = scan.nextLine();
+        System.out.println("Enter coordinate 2: i.e. (x, y)");
+        String second = scan.nextLine();
+
+        String x = first.substring(1, first.indexOf(","));
+        String y = first.substring(first.indexOf(" ") + 1, first.indexOf(")"));
+
+        String x2 = second.substring(1, second.indexOf(","));
+        String y2 = second.substring(second.indexOf(" ") + 1, second.indexOf(")"));
+
+        int firstXAsInt = Integer.parseInt(x);
+        int firstYAsInt = Integer.parseInt(y);
+        int secondXAsInt = Integer.parseInt(x2);
+        int secondYAsInt = Integer.parseInt(y2);
+
+        if (firstXAsInt == secondXAsInt) {
+            System.out.println("You have entered coordinates that result in a negative line");
+            System.out.println("The equation is x = " + firstXAsInt);
+        }
+
+        LinearEquation line = new LinearEquation(firstXAsInt, firstYAsInt, secondXAsInt, secondYAsInt);
+        System.out.println(line.slope());
+
+        System.out.println(firstXAsInt);
+        System.out.println(firstYAsInt);
+        System.out.println(secondXAsInt);
+        System.out.println(secondYAsInt);
+    }
 }
